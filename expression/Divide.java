ed9f064fcc13b2cc5ccd37c988560841715c1386
shelby57 <97250281+shelby57@users.noreply.github.com>
2022-03-02 22:06:13 +0300

    Add files via upload

---
+++ expression/Divide.java
@@ -0,0 +1,18 @@
+package expression;
+
+public class Divide extends Operation {
+
+    public Divide(CompositeExpression left, CompositeExpression right) {
+        super(left, right, "/");
+    }
+
+    @Override
+    public int evaluate(int x) {
+        return evaluate(x, 0, 0);
+    }
+
+    @Override
+    public int evaluate(int x, int y, int z) {
+        return left.evaluate(x, y, z) / right.evaluate(x, y, z);
+    }
+}
