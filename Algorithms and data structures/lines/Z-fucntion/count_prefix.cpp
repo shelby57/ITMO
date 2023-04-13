5e0e3d054186c3467328f00f07514c551fe8d970
Denis Rozhkov <97250281+shelby57@users.noreply.github.com>
2023-04-14 00:51:10 +0300

    Add files via upload

---
+++ Algorithms and data structures/lines/Z-fucntion/count_prefix.cpp
@@ -0,0 +1,56 @@
+/*
+ Задана строка s. Для каждого i от 1 до |s|
+ найдите количество вхождений в строку её префикса длины i.
+.
+ */
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> Z_function(const string &s) {
+    size_t n = s.size();
+    vector<int> z(s.size());
+    int l = 0, r = 0;
+    for (int i = 1; i < s.size(); i++) {
+        if (r >= i) {
+            z[i] = min(z[i - l], r - i + 1);
+        }
+        while (z[i] + i < n && s[z[i]] == s[z[i] + i]) {
+            z[i]++;
+        }
+        if (i + z[i] - 1 > r) {
+            l = i, r = i + z[i] - 1;
+        }
+    }
+    return z;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int q;
+    cin >> q;
+    while (q--) {
+        string s;
+        cin >> s;
+        vector<int> z = Z_function(s);
+        vector<int> result(s.size() + 1, 1);
+        for (int i: z) {
+            if (i != 0)
+                result[i] += 1;
+        }
+        int count = 0;
+        for (int i = result.size() - 1; i >= 0; --i) {
+            int prev = count;
+            if (result[i] != 1) {
+                count += result[i] - 1;
+            }
+            result[i] += prev;
+        }
+        for (int i = 1; i < result.size(); i++) {
+            cout << result[i] << " ";
+        }
+        cout << "\n";
+    }
+}
